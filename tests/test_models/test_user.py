8889fc26a40c1beb35ab122d686b4ab7e82cff79
simrett <leesim242@gmail.com>
2023-10-14 12:44:44 -0700

    modified tests

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,89 +1,105 @@
 #!/usr/bin/python3
-# test cases for base class
+"""
+test module for testing user models
+"""
+
+import datetime
 import unittest
 from models.base_model import BaseModel
 from models.user import User
-import models.base_model
-import models.user
-import inspect
-import datetime
-from time import sleep
 
 
-class TestUser(unittest.TestCase):
-    """ class to test user class """
+class TestUserModel(unittest.TestCase):
+    """test class for testing user models
+    """
     def setUp(self):
-        """This method is called before each test method in the test class.
+        self.temp_b = User()
+
+    def tearDown(self):
+        self.temp_b = None
+
+    def test_type(self):
+        """type checks for user model
         """
-        self.c = User()
+        self.assertEqual(issubclass(self.temp_b.__class__, BaseModel), True)
+        self.assertEqual(isinstance(self.temp_b, BaseModel), True)
+        self.assertEqual(isinstance(self.temp_b, User), True)
+        self.assertEqual(type(self.temp_b), User)
 
-    def test_doc_user(self):
-        """ test_doc(self): to test if module and class has docs """
-        self.assertIsNotNone(User.__doc__, 'no docs for Base class')
-        self.assertIsNotNone(models.user.__doc__, 'no docs for module')
-        for name, method in inspect.getmembers(User, inspect.isfunction):
-            self.assertIsNotNone(method.__doc__, f"{name} has no docs")
+    def test_basic_attribute_set(self):
+        """basic attribute assignment tests for user model
+        """
+        self.temp_b.name = "bennett"
+        self.temp_b.xyz = 400
+        self.assertEqual(self.temp_b.name, "bennett")
+        self.assertEqual(self.temp_b.xyz, 400)
 
-    def check_attributes(self):
-        """ check for attributes """
-        user = User()
-        user.first_name = "Jonas"
-        user.last_name = "stones"
-        user.email = "jonas@email.com"
-        user.password = "root"
-        self.assertEqual(user.first_name, "Jonas")
-        self.assertEqual(user.last_name, "stones")
-        self.assertEqual(user.email, "jonas@email.com")
-        self.assertEqual(user.password, "root")
-        self.assertTrue(hasattr(User, "first_name"))
-        self.assertTrue(hasattr(User, "last_name"))
-        self.assertTrue(hasattr(User, "password"))
-        self.assertTrue(hasattr(User, "email"))
+    def test_email(self):
+        """tests the email type of user
+        """
         self.assertEqual(type(User.email), str)
-        self.assertEqual(type(User.first_name), str)
-        self.assertEqual(type(User.last_name), str)
+
+    def test_password(self):
+        """tests the password type of user
+        """
         self.assertEqual(type(User.password), str)
 
-    def test_init_user(self):
-        """ test instantiation of class """
-        self.assertEqual(type(self.c.id), str)
-        self.assertEqual(type(self.c.updated_at), datetime.datetime)
-        self.assertEqual(type(self.c.created_at), datetime.datetime)
+    def test_first_name(self):
+        """tests the first_name type of user
+        """
+        self.assertEqual(type(User.first_name), str)
 
-    def test_save_user(self):
-        """ test User.save() """
-        current_updatedAt = self.c.updated_at
-        self.c.save()
-        self.assertNotEqual(current_updatedAt, self.c.updated_at)
+    def test_last_name(self):
+        """tests the last_name type of user
+        """
+        self.assertEqual(type(User.last_name), str)
 
-        """ test positional args """
-        with self.assertRaises(TypeError):
-            self.c.save(13)
+    def test_string_return(self):
+        """tests the string method to make sure it returns
+            the proper string
+        """
+        my_str = str(self.temp_b)
+        id_test = "[User] ({})".format(self.temp_b.id)
+        boolean = id_test in my_str
+        self.assertEqual(True, boolean)
+        boolean = "updated_at" in my_str
+        self.assertEqual(True, boolean)
+        boolean = "created_at" in my_str
+        self.assertEqual(True, boolean)
+        boolean = "datetime.datetime" in my_str
+        self.assertEqual(True, boolean)
 
-    def test_to_dict_city(self):
-        """ test User.to_dict() """
-        self.c.first_name = "jonas"
-        self.c.last_name = "stones"
-        self.c.email = "jonas@example.com"
-        self.c.password = "root"
-        dict1 = self.c.to_dict()
+    def test_to_dict(self):
+        """tests the to_dict method to make sure properly working
+        """
+        my_dict = self.temp_b.to_dict()
+        self.assertEqual(str, type(my_dict['created_at']))
+        self.assertEqual(my_dict['created_at'],
+                         self.temp_b.created_at.isoformat())
+        self.assertEqual(datetime.datetime, type(self.temp_b.created_at))
+        self.assertEqual(my_dict['__class__'],
+                         self.temp_b.__class__.__name__)
+        self.assertEqual(my_dict['id'], self.temp_b.id)
 
-        """ confirming the type of each attr in dict """
-        self.assertEqual(type(dict1['first_name']), str)
-        self.assertEqual(dict1['first_name'], "jonas")
-        self.assertEqual(dict1['last_name'], "stones")
-        self.assertEqual(dict1['email'], "jonas@example.com")
-        self.assertEqual(dict1['password'], "root")
-        self.assertEqual(type(dict1['__class__']), str)
-        self.assertEqual(dict1['__class__'], "User")
-        self.assertEqual(type(dict1['updated_at']), str)
-        self.assertEqual(type(dict1['id']), str)
-        self.assertEqual(type(dict1['created_at']), str)
+    def test_from_dict_basic(self):
+        """tests the from_dict method
+        """
+        my_dict = self.temp_b.to_dict()
+        my_base = User(**my_dict)
+        self.assertEqual(my_base.id, self.temp_b.id)
+        self.assertEqual(my_base.updated_at, self.temp_b.updated_at)
+        self.assertEqual(my_base.created_at, self.temp_b.created_at)
+        self.assertEqual(my_base.__class__.__name__,
+                         self.temp_b.__class__.__name__)
 
-        """ test positional args """
-        with self.assertRaises(TypeError):
-            self.c.to_dict('str')
-
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_from_dict_hard(self):
+        """tests from dict method of user inherited from base_model
+        """
+        self.temp_b.random = "hello!"
+        self.temp_b.z = 55
+        my_dict = self.temp_b.to_dict()
+        self.assertEqual(my_dict['z'], 55)
+        my_base = BaseModel(**my_dict)
+        self.assertEqual(my_base.z, self.temp_b.z)
+        self.assertEqual(my_base.random, self.temp_b.random)
+        self.assertEqual(my_base.created_at, self.temp_b.created_at)
